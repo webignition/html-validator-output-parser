6d47e134c2ac68fdaf26ae371e6f1a21f3147436
Jon Cram <jon@webignition.net>
2019-01-30 16:59:16 +0000

    Apply message filtering in main parser only

--- src/ApplicationJsonBodyParser.php
+++ src/ApplicationJsonBodyParser.php
@@ -2,7 +2,6 @@
 
 namespace webignition\HtmlValidatorOutput\Parser;
 
-use webignition\HtmlValidatorOutput\Models\ValidationErrorMessage;
 use webignition\ValidatorMessage\MessageList;
 
 class ApplicationJsonBodyParser
@@ -13,18 +12,12 @@ class ApplicationJsonBodyParser
         '& did not start a character reference. (& probably should have been escaped as &amp;.)';
 
     /**
-     * @var MessageExcluder
-     */
-    private $messageExcluder;
-
-    /**
      * @var MessageFactory
      */
     private $messageFactory;
 
-    public function __construct(Configuration $configuration)
+    public function __construct()
     {
-        $this->messageExcluder = MessageExcluderFactory::create($configuration);
         $this->messageFactory = new MessageFactory();
     }
 
@@ -43,10 +36,7 @@ class ApplicationJsonBodyParser
         foreach ($messages as $index => $messageValues) {
             if (is_array($messageValues)) {
                 $message = $this->messageFactory->createMessageFromArray($messageValues);
-
-                if ($message instanceof ValidationErrorMessage && !$this->messageExcluder->isExcluded($message)) {
-                    $messageList->addMessage($message);
-                }
+                $messageList->addMessage($message);
             }
         }
 
